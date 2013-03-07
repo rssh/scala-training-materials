694b16f39acd8d34f061436a6137998f926baeee
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2013-03-07 23:53:28 +0200

    change default confoguiration

--- simple-games/bargaining/src/main/scala/bargaining/Main.scala
+++ simple-games/bargaining/src/main/scala/bargaining/Main.scala
@@ -9,14 +9,15 @@ object Main {
     val agents = Seq(
                    new RandomAgent(),
                    new ProportionKeeper(0.5),
-                   //new ProportionKeeper(0.7),
-                   //new ProportionKeeper(0.8),
-                   //new ProportionKeeper(0.9),
+                   new ProportionKeeper(0.5),
+                   new ProportionKeeper(0.7),
+                   new ProportionKeeper(0.8),
+                   new ProportionKeeper(0.9),
                    new Scrooge(),
                    new YarsAgent()
                  )
     val runner = new GameRunner(agents,scala.util.Random.nextInt)
-    runner.printState
+    //runner.printState
     runner.playRandomGames(nGames=1000, sum=100)
     runner.printState
   }
